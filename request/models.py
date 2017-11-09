734342adcb2acb9f0c79d2391933a660a040a547
Balu Varanasi <vabasu@gmail.com>
2017-11-09 21:58:36 +0530

    add ip address using django-ipware

--- request/models.py
+++ request/models.py
@@ -9,6 +9,8 @@ from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
+from ipware.ip import get_real_ip
+
 from . import settings as request_settings
 from .managers import RequestManager
 from .utils import HTTP_STATUS_CODES, browsers, engines
@@ -63,7 +65,7 @@ class Request(models.Model):
         self.is_ajax = request.is_ajax()
 
         # User infomation
-        self.ip = request.META.get('REMOTE_ADDR', '')
+        self.ip = get_real_ip(request) or '127.0.0.1'
         self.referer = request.META.get('HTTP_REFERER', '')[:255]
         self.user_agent = request.META.get('HTTP_USER_AGENT', '')[:255]
         self.language = request.META.get('HTTP_ACCEPT_LANGUAGE', '')[:255]
